29bd87c048f059b5d70bb053fd6be092abd0f154
huangcancan-xbc <minbit2025@gmail.com>
2026-03-24 20:09:05 +0800

    不定参宏函数的使用

--- Prerequisites/1.不定参宏函数的使用.cc
+++ Prerequisites/1.不定参宏函数的使用.cc
@@ -46,19 +46,67 @@
 
 
 
+// #include <iostream>
+// using namespace std;
+
+// // #define LOG(format, ...) printf("[%s:%d]" format, __FILE__, __LINE__, __VA_ARGS__);
+// // 这会导致一个问题：但使用：LOG("小米里的大麦"); 会报错，原因是没有可变参数时，格式化中保留的“__LINE__,”这个逗号报错
+
+// // 解决办法：这里的 ##__VA_ARGS__ 会在 __VA_ARGS__ 为空时自动去掉多余的逗号
+// #define LOG(format, ...) printf("[%s:%d]" format, __FILE__, __LINE__, ##__VA_ARGS__);
+
+// int main()
+// {
+//     LOG("小米里的大麦\n");
+//     // 等价于：printf("[%s:%d]小米里的大麦\n", __FILE__, __LINE__);
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+// man va_arg: C语言中处理可变参数函数的四个宏/函数
+// #include <stdarg.h>                          // 头文件
+// void va_start(va_list ap, last);             // 初始化va_list，开始读取变参（必须第一个调用），参数：va_list变量、最后一个固定参数的名字
+// type va_arg(va_list ap, type);               // 取出下一个参数，并指定其类型（循环调用取参数），参数：va_list变量、期望的参数类型
+// void va_end(va_list ap);                     // 清理 va_list，结束变参读取（必须最后一个调用），参数：va_list变量
+// void va_copy(va_list dest, va_list src);     // 拷贝一份 va_list（C99+，需要“备份”参数列表时用），参数：目标va_list、源va_list
+
 #include <iostream>
+#include <stdarg.h>
 using namespace std;
 
-// #define LOG(format, ...) printf("[%s:%d]" format, __FILE__, __LINE__, __VA_ARGS__);
-// 这会导致一个问题：但使用：LOG("小米里的大麦"); 会报错，原因是没有可变参数时，格式化中保留的“__LINE__,”这个逗号报错
-
-// 解决办法：这里的 ##__VA_ARGS__ 会在 __VA_ARGS__ 为空时自动去掉多余的逗号
 #define LOG(format, ...) printf("[%s:%d]" format, __FILE__, __LINE__, ##__VA_ARGS__);
+
+// C语言中不定参数的使用，不定参数据的访问
+// count表示后面有多少个可变参数，然后逐个把这些参数按 int 取出来并打印
+void printNum(int count, ...)
+{
+    va_list ap;             // 定义一个变量，用来访问可变参数（我的理解是指针/句柄，官方解释叫游标）
+    va_start(ap, count);    // 表示找到可变参数的位置，而这里的count表示最后一个非可变参数，要从的他的后面开始读可变参数
+
+    for (int i = 0; i < count; i++)
+    {
+        // 从当前可变参数位置取出一个参数，按照 int 类型解释它，然后把 ap 移动到下一个参数，可以传不同的类型，但是要保证按正确类型读取
+        int num = va_arg(ap, int);
+
+        printf("参数：[%d]:%d\n", i, num);
+    }
+
+    va_end(ap);
+}
 
 int main()
 {
     LOG("小米里的大麦\n");
-    // 等价于：printf("[%s:%d]小米里的大麦\n", __FILE__, __LINE__);
+    printNum(2, 123, 123456);
+    printNum(5, 10, 20, 30, 40, 50);
 
     return 0;
 }
