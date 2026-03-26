e919e3375521cc248a4fd2b6d5e64f70a5f4202f
huangcancan-xbc <minbit2025@gmail.com>
2026-03-26 21:55:12 +0800

    日志等级类的定义

---
+++ source/level.hpp
@@ -0,0 +1,36 @@
+// 定义枚举类，枚举出日志等级
+// 提供转接口：将枚举转换成对应的字符串
+
+
+namespace mylog
+{
+    class LogLevel
+    {
+    public:
+        enum class value
+        {
+            UNKNOW = 0,
+            DEBUG,
+            INFO,
+            WARN,
+            ERROR,
+            FATAL,
+            OFF
+        };
+
+        const char *toString(LogLevel::value level)
+        {
+            switch(level)
+            {
+                case LogLevel::value::DEBUG : return "DEBUG";
+                case LogLevel::value::INFO : return "INFO";
+                case LogLevel::value::WARN : return "WARN";
+                case LogLevel::value::ERROR : return "ERROR";
+                case LogLevel::value::FATAL : return "FATAL";
+                case LogLevel::value::OFF : return "OFF";
+            }
+
+            return "UNKNOW";
+        }
+    };
+}
